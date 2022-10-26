feb19f607a3d8187165513e5a3c0f0ba0b71de9d
Thomas Huisman <contact@thohui.com>
2022-10-26 02:02:33 +0200

    Add heading section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, SimpleGrid, VStack } from "@chakra-ui/react";
 import { NextPage } from "next";
+import Head from "next/head";
 import { AuthWrapper } from "../components/auth/AuthWrapper";
 import { DashboardHeader } from "../components/dashboard/DashboardHeader";
 import { CreateEmailModal } from "../components/dashboard/email/CreateEmailModal";
@@ -13,37 +14,48 @@ import { useModal } from "../hooks/useModal";
 const Index: NextPage = () => {
   const { createMaskModal, createEmailModal, verifyEmailModal } = useModal();
   return (
-    <AuthWrapper>
-      <DashboardHeader />
-      <VStack h="100vh">
-        {createMaskModal.isOpen && (
-          <CreateMaskModal closeFn={createMaskModal.onClose} />
-        )}
-        {createEmailModal.isOpen && (
-          <CreateEmailModal closeFn={createEmailModal.onClose} />
-        )}
-        {verifyEmailModal.isOpen && (
-          <VerifyEmailModal
-            email={verifyEmailModal.email}
-            codeSent={verifyEmailModal.codeSent}
-            closeFn={verifyEmailModal.onClose}
-          />
-        )}
+    <>
+      <Head>
+        <title>Maskr</title>
+        <meta property="og:title" content="Maskr" key="title" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+      </Head>
+      <AuthWrapper>
+        <DashboardHeader />
+        <VStack h="100vh">
+          {createMaskModal.isOpen && (
+            <CreateMaskModal closeFn={createMaskModal.onClose} />
+          )}
+          {createEmailModal.isOpen && (
+            <CreateEmailModal closeFn={createEmailModal.onClose} />
+          )}
+          {verifyEmailModal.isOpen && (
+            <VerifyEmailModal
+              email={verifyEmailModal.email}
+              codeSent={verifyEmailModal.codeSent}
+              closeFn={verifyEmailModal.onClose}
+            />
+          )}
 
-        <SimpleGrid columns={1}>
-          <Box
-            boxSize={{
-              md: "4xl",
-              base: "100%",
-            }}
-          >
-            <MaskStats />
-            <Masks openModalFn={createMaskModal.onOpen} />
-            <Emails openModalFn={createEmailModal.onOpen} />
-          </Box>
-        </SimpleGrid>
-      </VStack>
-    </AuthWrapper>
+          <SimpleGrid columns={1}>
+            <Box
+              boxSize={{
+                md: "4xl",
+                base: "100%",
+              }}
+            >
+              <MaskStats />
+              <Masks openModalFn={createMaskModal.onOpen} />
+              <Emails openModalFn={createEmailModal.onOpen} />
+            </Box>
+          </SimpleGrid>
+        </VStack>
+      </AuthWrapper>
+    </>
   );
 };
 
