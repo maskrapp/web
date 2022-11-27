7b1f3670362cf7bb1375b43bedc4813f993a360e
Thomas Huisman <contact@thohui.com>
2022-11-27 18:19:11 +0100

    fix: token refreshing

--- hooks/useAxios.ts
+++ hooks/useAxios.ts
@@ -1,8 +1,15 @@
-import axios, { AxiosError } from "axios";
-import jwt_decode, { InvalidTokenError, JwtPayload } from "jwt-decode";
-import { Token } from "../types";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { Token, TokenPair } from "../types";
 import { BACKEND_URL } from "../utils/constants";
 import { useUser } from "./useUser";
+
+let refreshPromise: Promise<AxiosResponse<Token, any>> | null = null;
+
+const refreshAccessToken = async (refreshToken: string) => {
+  return axios.post(`${BACKEND_URL}/api/auth/refresh/`, {
+    refresh_token: refreshToken,
+  }) as Promise<AxiosResponse<Token, any>>;
+};
 
 export const useAxios = () => {
   const {
@@ -13,41 +20,61 @@ export const useAxios = () => {
 
   const axiosInstance = axios.create({
     baseURL: BACKEND_URL,
-    headers: { Authorization: `Bearer ${accessToken?.token}` },
   });
 
-  axiosInstance.interceptors.request.use(async (req) => {
-    try {
-      const decoded = jwt_decode<JwtPayload>(accessToken?.token ?? "");
-      const isExpired = Date.now() / 1000 > (decoded.exp ?? 0);
-      if (!isExpired) return req;
-      const response = await axios.post(`${BACKEND_URL}/api/auth/refresh/`, {
-        refresh_token: refreshToken?.token,
-      });
-      const newToken: Token = response.data;
-      const tokens = {
-        refresh_token: refreshToken,
-        access_token: newToken,
-      };
-      setAccessToken(newToken);
-      localStorage.setItem("tokens", JSON.stringify(tokens));
-      if (req.headers) {
-        req.headers.Authorization = `Bearer ${accessToken}`;
-      }
-    } catch (e: any) {
-      if (e.name === "AxiosError") {
-        // log out if token has expired
-        if ((e as AxiosError).response?.status === 401) {
-          localStorage.removeItem("tokens");
-          window.location.reload();
-        }
-      } else if (e instanceof InvalidTokenError) {
-        localStorage.removeItem("tokens");
-        window.location.reload();
-      }
-    } finally {
-      return req;
+  axiosInstance.interceptors.request.use((config) => {
+    if (accessToken) {
+      const tokens = localStorage.getItem("tokens");
+      const data: TokenPair = JSON.parse(tokens || "{}");
+      config.headers!["Authorization"] = `Bearer ${data.access_token.token}`;
     }
+    return config;
   });
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      const originalConfig = err.config;
+      if (err.response) {
+        if (err.response.status === 401 && !originalConfig._retry) {
+          originalConfig._retry = true;
+          if (refreshPromise) {
+            // if there's already a request being made to the refresh endpoint, then we just await that promise
+            await refreshPromise;
+            return axiosInstance(originalConfig);
+          }
+          try {
+            refreshPromise = refreshAccessToken(refreshToken!.token);
+            const rs = await refreshPromise;
+            const token = rs.data;
+            localStorage.setItem(
+              "tokens",
+              JSON.stringify({
+                refresh_token: refreshToken,
+                access_token: token,
+              })
+            );
+            setAccessToken(token);
+            return axiosInstance(originalConfig);
+          } catch (_error: any) {
+            if (_error.name === "AxiosError") {
+              // log out if token has expired
+              if ((_error as AxiosError).response?.status === 401) {
+                localStorage.removeItem("tokens");
+                window.location.assign("/");
+              }
+            }
+            return Promise.reject(_error);
+          } finally {
+            refreshPromise = null;
+          }
+        }
+      }
+
+      return Promise.reject(err);
+    }
+  );
+
   return axiosInstance;
 };
