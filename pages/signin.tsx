188a2e5b7bc3f15d34635a83f21d5ded793c3034
Thomas Huisman <contact@thohui.com>
2023-01-06 05:10:30 +0100

    fix: hydration errors on signin and signup page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,29 +1,40 @@
 import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { Signin } from "../components/auth/Signin";
 import { useUser } from "../hooks/useUser";
 
 const SignInPage: NextPage = () => {
-  const router = useRouter();
   const { isAuthenticated } = useUser();
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   if (isAuthenticated) {
     router.push("/");
     return null;
   }
-  const captchaKey = process.env.NEXT_PUBLIC_CAPTCHA_KEY ?? "";
-  return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={captchaKey}
-      scriptProps={{
-        appendTo: "head",
-      }}
-    >
-      <Flex minH={"100vh"} align={"center"} justify={"center"}>
-        <Signin />
-      </Flex>
-    </GoogleReCaptchaProvider>
-  );
+
+  if (mounted) {
+    const captchaKey = process.env.NEXT_PUBLIC_CAPTCHA_KEY ?? "";
+    return (
+      <GoogleReCaptchaProvider
+        reCaptchaKey={captchaKey}
+        scriptProps={{
+          appendTo: "head",
+        }}
+      >
+        <Flex minH={"100vh"} align={"center"} justify={"center"}>
+          <Signin />
+        </Flex>
+      </GoogleReCaptchaProvider>
+    );
+  }
+  return null;
 };
 export default SignInPage;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,33 +1,41 @@
 import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { SignUp } from "../components/auth/Signup";
 import { useUser } from "../hooks/useUser";
 
-// const reCaptchaKey =
-//   process.env.RECAPTCHA_KEY ?? "6LdEGNAiAAAAAEw7lkQqaRlOZgto6N2mMp0_wu2D";
-
 const SignUpPage: NextPage = () => {
   const router = useRouter();
   const { isAuthenticated } = useUser();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   if (isAuthenticated) {
     router.push("/");
     return null;
   }
-  const captchaKey = process.env.NEXT_PUBLIC_CAPTCHA_KEY ?? "";
-  return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={captchaKey}
-      scriptProps={{
-        appendTo: "head",
-      }}
-    >
-      <Flex id="flex" minH={"100vh"} align={"center"} justify={"center"}>
-        <SignUp />
-      </Flex>
-    </GoogleReCaptchaProvider>
-  );
+
+  if (mounted) {
+    const captchaKey = process.env.NEXT_PUBLIC_CAPTCHA_KEY ?? "";
+    return (
+      <GoogleReCaptchaProvider
+        reCaptchaKey={captchaKey}
+        scriptProps={{
+          appendTo: "head",
+        }}
+      >
+        <Flex id="flex" minH={"100vh"} align={"center"} justify={"center"}>
+          <SignUp />
+        </Flex>
+      </GoogleReCaptchaProvider>
+    );
+  }
+  return null;
 };
 
 export default SignUpPage;
