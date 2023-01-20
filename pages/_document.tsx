8f1f5a9f0e945332f58c5f02c42c0dbf60b73254
Thomas Huisman <contact@thohui.com>
2023-01-21 00:12:23 +0100

    feat: create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html dir="ltr">
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <body style={{ backgroundColor: "#232530" }}>
+        <ColorModeScript initialColorMode="dark" />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
