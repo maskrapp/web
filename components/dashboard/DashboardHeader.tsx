27d56c1c016d23b55d4fb4e4a2f708c3fe27c5ca
Thomas Huisman <contact@thohui.com>
2023-02-17 03:14:35 +0100

    feat: header rework

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,12 @@
-import { ChevronDownIcon, SettingsIcon } from "@chakra-ui/icons";
+import { FaMask } from "react-icons/fa";
+import { MdEmail, MdSettings } from "react-icons/md";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
   Badge,
+  Box,
   Button,
+  ButtonGroup,
+  Container,
   Flex,
   HStack,
   Link as ChakraLink,
@@ -11,9 +16,10 @@ import {
   MenuList,
   Text,
 } from "@chakra-ui/react";
-import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
 import { revokeRefreshToken } from "@/api/token";
+import { useMutation } from "@tanstack/react-query";
+import { JSXElementConstructor, ReactElement } from "react";
 
 export const Logo = () => {
   return (
@@ -29,92 +35,90 @@ export const DashboardHeader = () => {
     revokeRefreshToken(localStorage.getItem("refresh_token") ?? "")
   );
   return (
-    <Flex
-      justify="center"
+    <Box
       borderBottom={"1px solid"}
       bgColor={"blackAlpha.300"}
       borderColor="gray.300"
+      mb="8"
     >
-      <Flex
-        justify="space-between"
-        alignItems="center"
-        h="16"
-        maxW="1000px"
-        flex="1"
-      >
-        <HStack
-          spacing={{
-            md: 20,
-            base: "16",
-          }}
+      <Container maxW="container.lg">
+        <Flex
+          justify="space-between"
+          alignItems="center"
+          h="16"
+          maxW="1000px"
+          flex="1"
         >
-          <Logo />
           <HStack
+            w="full"
             spacing={{
-              base: 2,
-              md: 5,
+              md: 20,
+              base: "16",
             }}
           >
-            <NavItem name="Masks" href="/" />
-            <NavItem name="Emails" href="/emails" />
+            <Logo />
           </HStack>
-        </HStack>
-        <HStack>
-          <Menu placement="bottom-end">
-            <MenuButton as={Button} variant="outline" px="2">
-              <HStack>
-                <span>Actions</span>
-                <ChevronDownIcon />
-              </HStack>
-            </MenuButton>
-            <MenuList>
-              <Link href="/settings">
-                <MenuItem>
-                  <HStack>
-                    <SettingsIcon />
-                    <Text>Settings</Text>
-                  </HStack>
+          <HStack>
+            <Menu placement="bottom-end">
+              <MenuButton as={Button} variant="outline" px="2">
+                <HStack>
+                  <span>Actions</span>
+                  <ChevronDownIcon />
+                </HStack>
+              </MenuButton>
+              <MenuList>
+                <MenuItem
+                  color="red.400"
+                  onClick={async () => {
+                    await mutateAsync();
+                    localStorage.removeItem("access_token");
+                    localStorage.removeItem("refresh_token");
+                    window.location.assign("/");
+                  }}
+                >
+                  Sign out
                 </MenuItem>
-              </Link>
-              <MenuItem
-                color="red.400"
-                onClick={async () => {
-                  await mutateAsync();
-                  localStorage.removeItem("access_token");
-                  localStorage.removeItem("refresh_token");
-                  window.location.assign("/");
-                }}
-              >
-                Sign out
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </HStack>
-      </Flex>
-    </Flex>
+              </MenuList>
+            </Menu>
+          </HStack>
+        </Flex>
+        <Navigation />
+      </Container>
+    </Box>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <ButtonGroup>
+      <NavItem name="Masks" icon={<FaMask />} href="/" />
+      <NavItem name="Emails" icon={<MdEmail />} href="/emails" />
+      <NavItem name="Settings" icon={<MdSettings />} href="/settings" />
+    </ButtonGroup>
   );
 };
 
 interface NavItemProps {
   name: string;
   href: string;
+  icon?: ReactElement<any, string | JSXElementConstructor<any>>;
 }
 
-const NavItem = ({ name, href }: NavItemProps) => {
+const NavItem = ({ name, href, icon }: NavItemProps) => {
+  const selected = window.location.pathname === href;
   return (
     <Link href={href}>
-      <ChakraLink
-        px={2}
-        py={1}
-        rounded={"base"}
-        _hover={{
-          textDecoration: "none",
-          bg: "gray.700",
-        }}
+      <Button
+        as={ChakraLink}
+        leftIcon={icon}
+        variant="ghost"
+        rounded="sm"
         href={"#"}
+        _hover={{ textDecoration: "none", bgColor: "whiteAlpha.200" }}
+        borderBottom={selected ? "2px solid" : undefined}
       >
         {name}
-      </ChakraLink>
+      </Button>
     </Link>
   );
 };
