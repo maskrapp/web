5710f456762673fdddcd38a8717bfbf1cdb9f3f5
Thomas Huisman <contact@thohui.com>
2023-03-12 18:22:14 +0100

    fix: react query caching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,32 +3,32 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { UserContextProvider } from "@/contexts/UserContext";
 import Head from "next/head";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    },
+  },
+});
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+  breakpoints: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024",
+    xl: "1280",
+    "2xl": "1536px",
+  },
+};
+const theme = extendTheme({
+  config,
+});
+
 function App({ Component, pageProps }: AppProps) {
-
-  const config = {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-    breakpoints: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024",
-      xl: "1280",
-      "2xl": "1536px",
-    },
-  };
-
-  const theme = extendTheme({
-    config,
-  });
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-
   return (
     <>
       <Head>
