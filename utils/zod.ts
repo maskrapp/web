fcc5d5ef92dad4983e62679a6d71f5b96731a4dd
Thomas Huisman <contact@thohui.com>
2023-03-13 17:02:42 +0100

    refactor: remove unused schemas

--- utils/zod.ts
+++ utils/zod.ts
@@ -1,20 +1,9 @@
 import { z } from "zod";
-export const tokenRegex = new RegExp(
-  "^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$",
-);
 
-export const tokenSchema = z.string().regex(tokenRegex);
+export const tokenSchema = z
+  .string()
+  .regex(/^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/);
 
-const tokenObjectSchema = z.object({
-  token: tokenSchema,
-  expires_at: z.number(),
-  email_login: z.boolean(),
-});
-
-export const pairSchema = z.object({
-  access_token: tokenObjectSchema,
-  refresh_token: tokenObjectSchema,
-});
 export const verificationCodeSchema = z
   .string()
   .regex(new RegExp("^[0-9]{5}$"));
